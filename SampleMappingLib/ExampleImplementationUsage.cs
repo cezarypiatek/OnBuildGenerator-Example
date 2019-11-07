7549643fb8f6c80baaea28da7007de4293c04216
Cezary Piątek <cezarypiatek@live.com>
2019-11-07 06:50:32 +0100

    Add example for MappingGenerator.OnBuildGenerator

---
+++ SampleMappingLib/ExampleImplementationUsage.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleMappingLib
+{
+    class ExampleImplementationUsage
+    {
+        public void DoSomething()
+        {
+            var sampleMappingService = new SampleMapperService();
+            var userEntity = sampleMappingService.MapFrom(new UserDTO());
+        }
+    }
+}
